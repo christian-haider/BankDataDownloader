cb23508c9e09c3f816c8568a5da3664322d1bd9e
Stefan Haider <stefan@haider.io>
2016-04-28 17:53:47 +0200

    KeePass Wrapper for easy access of KeePass entries

---
+++ KeePass/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using KeePassLib;
+
+namespace KeePass
+{
+    public static class Extensions
+    {
+        public static string ToUtf8String(this byte[] bytes )
+        {
+            return System.Text.Encoding.UTF8.GetString(bytes);
+        }
+
+        public static string GetTitle(this PwEntry entry)
+        {
+            return entry.GetString("Title");
+        }
+
+        public static string GetUserName(this PwEntry entry)
+        {
+            return entry.GetString("UserName");
+        }
+
+        public static string GetPassword(this PwEntry entry)
+        {
+            return entry.GetString("Password");
+        }
+
+        public static string GetString(this PwEntry entry, string key)
+        {
+            return entry.Strings?.Get(key)?.ReadUtf8()?.ToUtf8String() ?? "";
+        }
+    }
+}
---
+++ KeePass/KeePassWrapper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using KeePassLib;
+using KeePassLib.Interfaces;
+using KeePassLib.Keys;
+using KeePassLib.Serialization;
+
+namespace KeePass
+{
+    public class KeePassWrapper : IDisposable
+    {
+        public string Path { get; private set; }
+        public PwDatabase Database { get; private set; }
+
+        private KeePassWrapper()
+        {
+
+        }
+
+        public static KeePassWrapper OpenWithPassword(string path, string password)
+        {
+            var wrapper = new KeePassWrapper
+            {
+                Path = path,
+                Database = new PwDatabase()
+            };
+            var io = IOConnectionInfo.FromPath(path);
+            var masterpass = new KcpPassword(password);
+            var compositeKey = new CompositeKey();
+            compositeKey.AddUserKey(masterpass);
+            wrapper.Database.Open(io, compositeKey, new NullStatusLogger());
+            return wrapper;
+        }
+
+        /// <summary>
+        /// finds the entry for the unique title
+        /// </summary>
+        /// <param name="title">Must be uniqu in the whole database</param>
+        /// <returns></returns>
+        public PwEntry GetEntryByTitle(string title)
+        {
+            
+            return GetAllEntries().SingleOrDefault(entry => entry.GetTitle()?.Equals(title) ?? false);
+        }
+
+        public PwEntry GetEntryByUuid(string uuidHex)
+        {
+            var uuid = new PwUuid(SoapHexBinary.Parse(uuidHex).Value);
+            return GetAllEntries().SingleOrDefault(entry => entry.Uuid.Equals(uuid));
+        }
+
+        public List<PwEntry> GetAllEntries()
+        {
+            return PwGroup.GetFlatEntryList(Database.RootGroup.GetFlatGroupList()).ToList();
+        } 
+
+        public void Dispose()
+        {
+            Database.Close();
+        }
+    }
+}
