5ddee40a70755fb92915d5d22526db121597c6a3
Stefan Haider <stefan@haider.io>
2016-06-03 11:56:20 +0200

    UI: first windows

---
+++ DataDownloader.Ui/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace DataDownloader.Ui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+        }
+
+        private void MenuItemSettings_Click(object sender, RoutedEventArgs e)
+        {
+            new SettingWindow().ShowDialog();
+        }
+
+        private void ButtonStartDownload_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
