909047f943cbf5f9e43e73e4d4d3f44a605ed5cd
Stefan Haider <stefan@haider.io>
2017-01-13 20:08:18 +0100

    About + icons for windows

--- DataDownloader.Ui/Properties/AssemblyInfo.cs
+++ DataDownloader.Ui/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Resources;
+﻿using System;
+using System.Resources;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
@@ -6,12 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Data Downloader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Data Downloader UI")]
+[assembly: AssemblyDescription("Bank Data Downloader is an application to download bank transactions and statements from different banks via Selenium webdriver.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("haider.io")]
 [assembly: AssemblyProduct("Bank Data Downloader")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © Stefan Haider 2016-2017.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,7 +50,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.*")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
--- DataDownloader.Ui/Windows/AboutWindow.xaml.cs
+++ DataDownloader.Ui/Windows/AboutWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Deployment.Application;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,6 +13,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DataDownloader.Ui.Windows
@@ -21,7 +25,37 @@ namespace DataDownloader.Ui.Windows
     {
         public AboutWindow()
         {
+            ReadAssemblyInfo();
             InitializeComponent();
+        }
+
+        public string ProductVersion { get; set; } = "1.0.0.0";
+        public string AssemblyVersion { get; set; }
+        public string DeploymentVersion { get; set; }
+        public string Version => DeploymentVersion ?? ProductVersion ?? AssemblyVersion;
+        public string Copyright { get; set; }
+        public string Product { get; set; }
+        public string Description { get; set; }
+
+        private void ReadAssemblyInfo()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            ProductVersion = versionInfo.ProductVersion;
+            AssemblyVersion = assembly.GetName().Version.ToString();
+            if (ApplicationDeployment.IsNetworkDeployed)
+            {
+                DeploymentVersion = ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString(4);
+            }
+            Copyright = versionInfo.LegalCopyright;
+            Product = versionInfo.ProductName;
+            Description = versionInfo.Comments;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
